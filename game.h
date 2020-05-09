e814391725c63d90fb9ff612d850eb7e8216e042
tracy666 <61071994+tracy666@users.noreply.github.com>
2020-05-09 17:57:33 +0800

    Create game.h

---
+++ game.h
@@ -0,0 +1,24 @@
+#ifndef factorial_H
+#define factorial_H
+int factorial(int n);
+#endif
+#ifndef permutation_H
+#define permutation_H
+int permutation(int n, int r);
+#endif
+#ifndef combination_H
+#define combination_H
+int combination(int n, int r);
+#endif
+#ifndef subtract_H
+#define subtract_H
+int subtract(int a, int b);
+#endif
+#ifndef bigger_H
+#define bigger_H
+int bigger(int a, int b);
+#endif
+#ifndef smaller_H
+#define smaller_H
+int smaller(int a, int b);
+#endif
